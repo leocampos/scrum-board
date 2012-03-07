324007bb718b8f2a5dccbf25d5fe9c41e47d7d76
Leonardo Campos <leonardo.campos@abril.com.br>
2012-03-07 16:23:09 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScrumBoard::Application.config.secret_token = '6c596525e0ad8156be43dda058f88121d2e9292794444aaf79dc64c88eb920f031c1555dc96b67c972b5465584d0fdccff311184c829327b8fad0040e565635c'
