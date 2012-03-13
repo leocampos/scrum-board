97ead7f49a131f00ef206f7ec0c72c69da77995d
Leonardo Campos <leonardo.campos@abril.com.br>
2012-03-13 12:33:50 -0300

    Adding Twitter bootstrap to project

--- spec/lib/scrum_board/rally_client_spec.rb
+++ spec/lib/scrum_board/rally_client_spec.rb
@@ -36,24 +36,4 @@ describe ScrumBoard::RallyClient do
       end
     end
   end
-  
-  # def connect
-  #   @connector = RallyConnector.new(:username => rally_configuration[:username], :password => rally_configuration[:password])
-  #   @conn = @connector.connect
-  #   @workspace = @connector.workspace
-  # end
-  # 
-  # def find_team_by_name(name)
-  #   #connector.workspace.projects[7].iterations.find {|sprint| sprint.name == 'Sprint #53'}.end_date
-  #   #connector.workspace.projects[7].iterations.find {|sprint| sprint.name == 'Sprint #54'}.state
-  #   @workspace.projects.find do |project|
-  #     project.name.downcase == name
-  #   end
-  # end
-  # 
-  # private
-  # def rally_configuration
-  #   @rally_configuration ||= ScrumBoard::Yml.read_from_config("rally_account")
-  #   @rally_configuration
-  # end
 end
